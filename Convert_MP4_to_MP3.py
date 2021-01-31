b6681e8010537214e0e7d32bc57e8ec7e2612c28
黃敏雄 <64833384+MinXiong-Huang@users.noreply.github.com>
2021-01-31 12:44:14 +0800

    Update Convert_MP4_to_MP3.py

--- Convert_MP4_to_MP3.py
+++ Convert_MP4_to_MP3.py
@@ -2,9 +2,9 @@ import easygui
 from moviepy.editor import *
 
 Video_url = easygui.fileopenbox() #選擇Video檔案路徑
-FileName=Video_url.split('\\') #字串切割
-Music_Name=FileName[-1] 
-Music_Name=Music_Name[:-4] #音樂檔名
+FileName = Video_url.split('\\') #字串切割
+Music_Name = FileName[-1] 
+Music_Name = Music_Name[:-4] #音樂檔名
 
 video = VideoFileClip(Video_url)
 video.audio.write_audiofile('{}.mp3'.format(Music_Name)) #Convert MP4 to MP3
